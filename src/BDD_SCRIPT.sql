582e228b42c72f9e5fa9e06a8ca39e3e19b0ef28
Dylan Delacote <dyland25@hotmail.fr>
2018-06-26 13:38:50 +0200

    LE COMMIT DE SES MORTS LE JO !

--- src/BDD_SCRIPT.sql
+++ src/BDD_SCRIPT.sql
@@ -63,6 +63,13 @@ insert into reserve (personneID,salleID,duree,dateDebut,dateFin,code,status) val
 insert into reserve (personneID,salleID,duree,dateDebut,dateFin,code,status) values (4,2,30,now() - interval'1 hour',now() + interval'30 minutes',456,2);
 insert into reserve (personneID,salleID,duree,dateDebut,dateFin,code,status) values (4,3,30,now(),now() + interval'2 hours',123,1);*/
 
+insert into TYPESALLE(nom) values ('Simple');
+insert into TYPESALLE(nom) values('Classique');
+insert into TYPESALLE(nom) values ('Audio et web conférence');
+insert into TYPESALLE(nom) values('Classique audition');
+insert into TYPESALLE(nom) values ('Salle spécifique aux handicap visuels');
+insert into TYPESALLE(nom) values('Salle spécifique avec logiciels spécifiques');
+
 /*PROCEDURE TO DELETE RESERVATION 15 minutes after starting if no one in it*/
 CREATE OR REPLACE FUNCTION check_reservation(duration interval) RETURNS void AS'
 	DELETE FROM reserve where dateDebut <= (now()-duration) and status = 1;' LANGUAGE SQL;
